636e78ee05be7d60ebc6f21033a73a9242c1a36c
Macintosh HD <thienhamaimai1@gmail.com>
2019-05-02 15:04:35 +0700

    123

--- model/config.js
+++ model/config.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('jassis_core_database','jteam','f0r3v3r4l0n3@1403',{
-    dialect:'mssql',
-   host:'jcenter.sytes.net',
-   port:1433,
-   encrypt:false,
-  
-});
+const sequelize = new Sequelize('jassis_core_database', 'futuresky', 'f0r3v3r4l0n3@1403', {
+    dialect: 'mssql',
+    dialectOptions: {
+        options: {
+            encrypt: true
+        }
+    },
+    host: 'jassis.database.windows.net',
+    port: 1433,
+})
 module.exports = sequelize;
